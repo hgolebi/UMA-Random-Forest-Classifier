5083e35b2fbbc839a6da96950e25876c27dee888
hgolebi <hugo7744@gmail.com>
2023-10-30 20:18:48 +0100

    divided files into separate catalogs

--- testes.py
+++
@@ -1,6 +0,0 @@
-# autorzy: Hubert Gołębiowski, Jakub Rozkosz
-
-from sklearn.datasets import make_classification
-X, y = make_classification(n_samples=1000, n_features=4,
-                           n_informative=2, n_redundant=0,
-                           random_state=0, shuffle=False)
